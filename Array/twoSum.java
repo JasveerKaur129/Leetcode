b9a50382aa938bb06508d9dbfca7ac8bbdb7af21
JasveerKaur129 <88249162+JasveerKaur129@users.noreply.github.com>
2021-10-19 16:22:55 +0530

    Two Sum

---
+++ Array/twoSum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j < nums.length ; j++){
+                int sum = nums[i] + nums[j];
+                
+                if(sum == target){
+                    int[] ans = new int[]{i,j};
+                   
+                    // return new int[]{i,j};
+                    return ans;
+                }
+            }
+            
+        }
+         return new int[] {};  //  return null;
+    }
+    
+}
