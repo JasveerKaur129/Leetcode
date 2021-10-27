a70e798e9edb45267403d3ec759154c81dc0ac5b
JasveerKaur129 <88249162+JasveerKaur129@users.noreply.github.com>
2021-10-27 21:11:20 +0530

    Valid Parentheses

---
+++ Stack/validParentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+       Stack<Character> st= new Stack<>();
+        
+        for(int i=0;i<s.length();i++){
+            
+            char ch =s.charAt(i);
+            
+            if(ch=='['|| ch=='{'||ch=='('){
+                st.push(ch);
+            }else if(ch==']'|| ch=='}'||ch==')'){
+                if(st.size() == 0){
+                    return false;
+                }if(ch==']'){
+                    if(st.peek()!='['){
+                    return false;
+                    }
+                }else if(ch=='}'){
+                    if(st.peek()!='{'){
+                    return false;
+                    }
+                } else if(ch==')'){
+                    if(st.peek()!='('){
+                    return false;
+                    }
+                }
+                st.pop();
+            }
+        }
+        if(st.size()==0){
+            return true;
+            
+        }else{
+            return false;
+        }
+    }
+}
